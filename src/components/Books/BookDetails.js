89db321628abd7957f06cf351cb5896c19223dea
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2021-11-16 14:28:48 +0200

    Building the details page

--- src/components/Books/BookDetails.js
+++ src/components/Books/BookDetails.js
@@ -9,16 +9,27 @@ const BookDetails = ({ onClose, book }) => {
                 <div className={styles['book-image']}>
                     <img src={book.imageUrl} alt={book.title} />
                 </div>
-                <div className={styles['book-title']}>{book.title}</div>
-                <div className={styles['book-author']}>{book.author}</div>
-                <div className={styles['book-description']}>
-                    {book.description}
+                <div className={styles['book-content']}>
+                    <div className="book-text">
+                        <div className={styles['book-title']}>{book.title}</div>
+                        <div className={styles['book-author']}>
+                            {book.author}
+                        </div>
+                        <div className={styles['book-description']}>
+                            {book.description}
+                        </div>
+                    </div>
+                    <div className={styles.buttons}>
+                        <div className={styles['main-actions']}>
+                            <button className="btn btn-primary">Edit</button>
+                            <button className="btn btn-danger">Delete</button>
+                        </div>
+                        <button className="btn btn-primary" onClick={onClose}>
+                            Close
+                        </button>
+                    </div>
                 </div>
             </article>
-
-            <button onClick={onClose}>Close</button>
-            <button>Edit</button>
-            <button>Delete</button>
         </Modal>
     );
 };
