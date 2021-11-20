01b21b3275d1545590c3293bf66bd820e792bf5f
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2021-11-20 13:21:48 +0200

    Fixing minor bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Fragment } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-import Header from './components/Layout/Header';
 import AddNewBook from './components/Books/AddNewBook';
+import Header from './components/Layout/Header';
 import Books from './components/Books/Books';
 import BookDetails from './components/Books/BookDetails';
 import Footer from './components/Layout/Footer';
+import EditBook from './components/Books/EditBook';
 
 function App() {
     return (
@@ -27,13 +28,23 @@ function App() {
                     }
                 />
                 <Route
+                    path="/books/:bookId/edit"
+                    element={
+                        <>
+                            <Books />
+                            <EditBook />
+                        </>
+                    }
+                />
+                <Route
                     path="/add-new-book"
                     element={
                         <>
                             <Books />
                             <AddNewBook />
                         </>
-                    }></Route>
+                    }
+                />
             </Routes>
             <Footer />
         </Fragment>
