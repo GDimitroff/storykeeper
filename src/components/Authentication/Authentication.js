c3b15220148ceaefc1df66207d47897b36ad13b5
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2021-11-24 12:31:30 +0200

    Merging login and register components

--- src/components/Authentication/Authentication.js
+++ src/components/Authentication/Authentication.js
@@ -2,12 +2,28 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import Modal from '../UI/Modal';
-import Login from './Login';
-import Register from './Register';
+import styles from './Authentication.module.css';
 
 const Authentication = () => {
-    const [isLogin, setIsLogin] = useState(true);
     const navigate = useNavigate();
+    const [isLogin, setIsLogin] = useState(true);
+
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredUsernameIsInvalid, setEnteredUsernameIsInvalid] =
+        useState(false);
+    const [enteredPassword, setEnteredPassword] = useState('');
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    };
+
+    const onFocusHandler = () => {
+        setEnteredUsernameIsInvalid(false);
+    };
+
+    const passwordChangeHandler = (event) => {
+        setEnteredPassword(event.target.value);
+    };
 
     const onSwitchHandler = () => {
         setIsLogin((prevState) => !prevState);
@@ -17,13 +33,64 @@ const Authentication = () => {
         navigate('/');
     };
 
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        if (enteredUsername === '') {
+            setEnteredUsernameIsInvalid(true);
+            return;
+        }
+
+        setEnteredUsername('');
+        setEnteredPassword('');
+
+        navigate('/');
+    };
+
     return (
         <Modal modalType="auth" onClose={onCloseHandler}>
-            {isLogin ? (
-                <Login onSwitch={onSwitchHandler} />
-            ) : (
-                <Register onSwitch={onSwitchHandler} />
-            )}
+            <h1 className={styles.heading}>{isLogin ? 'Login' : 'Sign Up'}</h1>
+            <form
+                onSubmit={submitHandler}
+                className={styles.form}
+                onFocus={onFocusHandler}>
+                <div
+                    className={`${styles['form-control']} ${
+                        enteredUsernameIsInvalid ? styles['invalid'] : ''
+                    }`}>
+                    <label htmlFor="email">E-mail</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={enteredUsername}
+                        onChange={usernameChangeHandler}
+                    />
+                </div>
+
+                <div className={styles['form-control']}>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={enteredPassword}
+                        onChange={passwordChangeHandler}
+                    />
+                </div>
+
+                {enteredUsernameIsInvalid && (
+                    <p className={styles['error-text']}>Error message</p>
+                )}
+
+                <button className={styles.btn}>
+                    {isLogin ? 'Login' : 'Register'}
+                </button>
+            </form>
+            <p className={styles['text']}>
+                {isLogin ? 'Not registered?' : 'Already have an account?'}{' '}
+                <span className={styles['switch']} onClick={onSwitchHandler}>
+                    {isLogin ? 'Create an account' : 'Login'}
+                </span>
+            </p>
         </Modal>
     );
 };
